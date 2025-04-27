3970a9b900ad5affa3fa9c5d8a9e504b8ae77d44
GouthamSomaraju <mr.goutham00@gmail.com>
2025-04-27 23:41:26 +0530

    About.jsxd

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { Card, Row, Col, Container } from "react-bootstrap";
 
-import van from "./Assets/images/about/van.jpg";
+import van from "./assets/images/about/van.jpg"
 import destination from "./assets/images/about/destination.png";
 import best from "./assets/images/about/best-price.png";
 import quick from "./assets/images/about/quick.png";
 
+
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+
 
 const about = [
   {
